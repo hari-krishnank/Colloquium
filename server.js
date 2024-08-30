a896a23f0fb1f8d50132f4aa825ca281be8f54c9
Harikrishnan <hkharikrishnan23@gmail.com>
2024-08-30 12:03:05 +0530

    all modifications removed

--- server.js
+++ server.js
@@ -3,25 +3,20 @@ const http = require('http');
 const path = require("path");
 
 const app = express();
-const server = http.createServer(app);
+const server = http.createServer(app); 
 
-const io = require("socket.io")(server, {
-    cors: {
-        origin: '*',
-        methods: ['GET', 'POST']
-    }
-});
+const io = require("socket.io")(server);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-io.on("connection", function (socket) {
-    socket.on("newuser", function (username) {
+io.on("connection", function(socket) {
+    socket.on("newuser",function(username) {
         socket.broadcast.emit("update", username + " joined the conversation")
     })
-    socket.on("exituser", function (username) {
+    socket.on("exituser",function(username) {
         socket.broadcast.emit("update", username + " left the conversation")
     })
-    socket.on("chat", function (message) {
+    socket.on("chat",function(message) {
         socket.broadcast.emit("chat", message)
     })
 })
